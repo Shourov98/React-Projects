13b1d4c8be668ef2e86f99c7d9d30676d4c044cf
mrgroot <mrgroot176@gmail.com>
2023-10-11 13:17:10 +0600

    nike-projrct

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Nevigation from './components/Nevigation';
+import Hero from './components/Hero';
+
+const App = () => {
+  return (
+    <div>
+      <Nevigation />
+      <Hero />
+    </div>
+  )
+}
+
+export default App;
